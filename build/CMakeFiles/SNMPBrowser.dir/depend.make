85154f0c66a195007050e279ac745c4acaf4cb9a
Phoenix-flame <alireza17010@gmail.com>
2026-05-28 06:57:36 +0330

    fix: removed build directory

--- build/CMakeFiles/SNMPBrowser.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for SNMPBrowser.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/SNMPBrowser.dir/qrc_qml.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/SNMPBrowser.dir/qrc_qml.cpp.o: \
- /home/alireza/Programming/SNMP_Browser/cpp_qt/build/qrc_qml.cpp \
- /usr/include/stdc-predef.h
